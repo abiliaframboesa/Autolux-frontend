0665d050988d9fa2ebfe0896493ceef1643d7550
abiliaframboesa <abiliaframboesa@gmail.com>
2026-04-18 00:27:14 +0100

    adiciona SearchBar

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,10 @@
+function SearchBar() {
+    return (
+        <div className="search-bar">
+            <input type="text" placeholder="Pesquisar..." />    
+            {/* <button>Pesquisar</button> */}
+        </div>
+    )
+}   
+
+export default SearchBar;
