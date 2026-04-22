590fd0337c3cbec97df0aea643c05bc799bcf719
abiliaframboesa <abiliaframboesa@gmail.com>
2026-04-22 15:08:51 +0100

    atualiza catalogo

--- src/pages/Catalogo.jsx
+++ src/pages/Catalogo.jsx
@@ -1,5 +1,27 @@
+import vehicles from '../data/vehicles.json';
+import VehicleCard from '../components/VehicleCard';
+
 function Catalogo() {
-    return <h1>Catálogo</h1>
+    return (
+        <div>
+            <h1>Catálogo</h1>
+            {vehicles.map((v) => (
+                <VehicleCard key={v.id} {...v} />
+            ))}
+        </div>
+    )
 }
 
 export default Catalogo;
+
+
+
+// {/* {vehicles.map((v) => (
+//     <div key={v.id} style={{ border: "1px solid #ccc", margin: 10, padding: 10 }}>
+//         <img src={v.imagem} alt={v.modelo} width="300" />
+
+//         <h2>{v.marca} {v.modelo}</h2>
+//         <p>{v.ano}</p>
+//         <p><strong>{v.preco} €</strong></p>
+//     </div>
+// ))} */}
