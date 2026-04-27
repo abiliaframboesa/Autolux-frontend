bba414abbaded2d6ca1cd57de63cb4148b2d917d
abiliaframboesa <abiliaframboesa@gmail.com>
2026-04-27 20:25:18 +0100

    adiciona estilos para cartao do veiculo

--- src/components/VehicleCard.jsx
+++ src/components/VehicleCard.jsx
@@ -3,7 +3,7 @@ function VehicleCard(props) {
         <div className="vehicle-card">
             <img src={props.imagem} alt={props.modelo} width="300" />
             <h2>{props.marca} {props.modelo}</h2>
-            <p>{props.ano}</p>
+            <p>{props.ano} - {props.combustivel}</p>
             <p><strong>{props.preco} €</strong></p>
         </div>
     )
