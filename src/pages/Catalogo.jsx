4daeaabea11a6472c67f57d200cf8ebfd784bdc9
abiliaframboesa <abiliaframboesa@gmail.com>
2026-04-27 20:22:46 +0100

    adiciona filtros dos carros

--- src/pages/Catalogo.jsx
+++ src/pages/Catalogo.jsx
@@ -1,12 +1,60 @@
+import { useState } from 'react';
 import vehicles from '../data/vehicles.json';
 import VehicleCard from '../components/VehicleCard';
 import '../components/VehicleCard.css';
 
 function Catalogo() {
+    const [marca, setMarca] = useState("");
+    const [combustivel, setCombustivel] = useState("");
+    const [precoMax, setPrecoMax] = useState("");
+
+    const filtrados = vehicles.filter((v) => {
+        return (
+            (marca === "" || v.marca === marca) &&
+            (combustivel === "" || v.combustivel === combustivel) &&
+            (precoMax === "" || v.preco <= Number(precoMax))
+        );
+    });
     return (
         <div>
             <h1>Catálogo</h1>
-            {vehicles.map((v) => (
+            
+            {/* FILTROS */}
+            <div>
+                <select onChange={(e) => setMarca(e.target.value)}>
+                    <option value="">Todas as marcas</option>
+                    <option value="BMW">BMW</option>
+                    <option value="Mercedes-Benz">Mercedes-Benz</option>
+                    <option value="Toyota">Toyota</option>
+                    <option value="Tesla">Tesla</option>
+                    <option value="Volkswagen">Volkswagen</option>
+                    <option value="Volvo">Volvo</option>
+                    <option value="Peugeot">Peugeot</option>
+                    <option value="Renault">Renault</option>
+                    <option value="Audi">Audi</option>
+                    <option value="Ford">Ford</option>
+                    <option value="SEAT">SEAT</option>
+                </select>
+
+                <select onChange={(e) => setCombustivel(e.target.value)}>
+                    <option value="">Todos combustíveis</option>
+                    <option value="Gasolina">Gasolina</option>
+                    <option value="Diesel">Diesel</option>
+                    <option value="Híbrido">Híbrido</option>
+                    <option value="Híbrido Plug-in">Híbrido Plug-in</option>
+                    <option value="Elétrico">Elétrico</option>
+                </select>
+
+                <input
+                    type="number"
+                    placeholder="Preço máximo"
+                    onChange={(e) => setPrecoMax(e.target.value)}
+                />
+            </div>
+
+
+            {/* MAPEAR VEÍCULOS */}
+            {filtrados.map((v) => (
                 <VehicleCard key={v.id} {...v} />
             ))}
         </div>
