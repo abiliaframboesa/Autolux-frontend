dd2fea547388ad614f54b257c370d22005e40a95
abiliaframboesa <abiliaframboesa@gmail.com>
2026-05-16 21:58:45 +0100

    adiciona botao favoritos

--- src/components/VehicleCard.jsx
+++ src/components/VehicleCard.jsx
@@ -1,12 +1,27 @@
-function VehicleCard(props) { 
+import { Link } from "react-router-dom";
+
+function VehicleCard(props) {
+
+    const favorito = props.favoritos?.find(
+        (v) => v.id === props.id
+    );
+
     return (
         <div className="vehicle-card">
-            <img src={props.imagem} alt={props.modelo} width="300" />
-            <h2>{props.marca} {props.modelo}</h2>
-            <p>{props.ano} - {props.combustivel}</p>
-            <p><strong>{props.preco} €</strong></p>
+
+            <Link to={`/veiculo/${props.id}`}>
+                <img src={props.imagem} alt={props.modelo} width="300" />
+                <h2>{props.marca} {props.modelo}</h2>
+                <p>{props.ano} - {props.combustivel}</p>
+                <p><strong>{props.preco} €</strong></p>
+            </Link>
+
+            <button onClick={() => props.toggleFavorito(props)}>
+                {favorito ? "Remover Favorito" : "Adicionar Favorito"}
+            </button>
+
         </div>
-    )
+    );
 }
 
 export default VehicleCard;
